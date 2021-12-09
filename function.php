761be13e6c9778f4f67240ac1470f338fdfddd7e
Takuma Kanazawa <eennyade.1211.t@gmail.com>
2021-12-09 19:21:01 +0900

    session modifeid

--- function.php
+++ function.php
@@ -30,12 +30,10 @@ function debugLogStart(){
 //セッション準備・セッションの有効期限を延ばす
 //==========================================
 //RedisTOGOへ接続
-$redis_url = "tcp://" . parse_url($_ENV['REDISTOGO_URL'], PHP_URL_HOST) . ":" . parse_url($_ENV['REDISTOGO_URL'], PHP_URL_PORT);
-if (!is_array(parse_url($_ENV['REDISTOGO_URL'], PHP_URL_PASS))) {
-  $redis_url .= "?auth=" . parse_url($_ENV['REDISTOGO_URL'], PHP_URL_PASS);
-}
+$url = parse_url(getenv('REDISTOGO_URL'));
+$redisServer = sprintf('tcp://%s:%d?auth=%s', $url['host'], $url['port'], $url['pass']);
 ini_set("session.save_path", $redis_url);
-ini_set("session.save_handler", "redis");
+ini_set("session.save_handler", $redisSever);
 //セッションファイルの置き場所を変更する
 //session_save_path("C:\WINDOWS\Temp");
 //ガベージコレクションが削除するセッションの有効期限を設定（30日以上経過しているものに対してのみ１００分の１の確率で削除）
